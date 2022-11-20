26114a5ce27a8a531d3989561a5ace6300e17776
zorgensk <zorgensk@gmail.com>
2022-11-20 21:39:55 +0700

    task1

---
+++ Homework_task1/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B)
+// и возводит число A в натуральную степень B.
+
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+Console.WriteLine("Введите число");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите степень числа,  в которую нужно возвести");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int DegreeOfNumber(int a , int b)
+{
+    int res = 1;
+
+    for(int i = 1; i <= b; i++)
+    {
+        res *= a;
+    }
+    return res;
+}
+
+ Console.WriteLine($"Ответ: {DegreeOfNumber(a , b)}");
+ 
